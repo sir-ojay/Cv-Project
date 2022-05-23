75e74895c91157368ffe6550cb0adc2cec536767
Ibrahim Sannu <prince.ibrahim76@gmail.com>
2022-05-23 01:02:54 -0700

    Update Summary.js
    summary  review

--- src/components/rightColumn/Summary.js
+++ src/components/rightColumn/Summary.js
@@ -8,7 +8,7 @@ function Summary() {
          {/* Experience start */}
           <div className="summary-text">
          
-          <p> A front end developer passionate about coding pixel-perfect web platforms. I develop  quality websites with Reactjs.</p>
+          <p>A frontend developer who loves the artistic combination of code and designs, who is also passionate about coding pixel-perfect web platforms. I like building user centered applications so i am passionate in making the web easily accessible for everyone. I work with teams to create high performance and rich interactive websites that work across all platforms and devices. I have a taste for clean code and design, most especially love to find bugs and fix them.</p>
         
           </div> {/* Experience end */}
         </div>
