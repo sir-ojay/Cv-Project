4ad8daa6dc2b6203a16ff2a3134788ce7274495e
Ibrahim Sannu <prince.ibrahim76@gmail.com>
2022-05-23 02:19:39 -0700

    Update Footer.js
    link resolved

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,9 +7,9 @@ export default function Footer() {
         <p className='social-info'>Find me on my social media handles.</p>
      
         <span className="socilas">
-        <i class="fa fa-github" aria-hidden="true"><a href="https://github.com/sir-ojay" target="_blank">github</a></i>
-          <i className="fa fa-twitter-square"><a href="https://twitter.com/ibrahimsannu" target="_blank">Twitter</a></i>
-          <i className="fa fa-linkedin-square"><a href="https://www.linkedin.com/in/ibrahim-ojeabu-sannu-b5bb70149/" target="_blank">LinkedIn</a></i>
+        <i class="fa fa-github" aria-hidden="true"><a href="https://github.com/sir-ojay" target="_blank" rel="noopener" >github</a></i>
+          <i className="fa fa-twitter-square"><a href="https://twitter.com/ibrahimsannu" target="_blank" rel="noopener">Twitter</a></i>
+          <i className="fa fa-linkedin-square"><a href="https://www.linkedin.com/in/ibrahim-ojeabu-sannu-b5bb70149/" target="_blank" rel="noopener">LinkedIn</a></i>
         </span>
         <p className='poweredby'>Powered by Ojay</p>
       </footer>
